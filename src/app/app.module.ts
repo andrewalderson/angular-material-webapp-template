3fc033c6cc35d6f50137aa4f9b58ff925c4f29f8
Andrew Alderson <andrew@andrewalderson.com>
2017-06-14 11:44:22 -0600

    created initial layout - created app header component - created sidenav component - set up css for full screen layout - added angular material theme - added fonts to index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from "@angular/material";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppSidenavComponent } from './components/app-sidenav/app-sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppSidenavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
